36aed356fd4c3982d1a1d3fa69fbc9337530aa14
Isaque Keller <kellerisaque@gmail.com>
2026-04-23 17:36:16 -0300

    Update Album.java

--- 2.6 - Associação + Gerenciador + SRP/Ex 8/Album.java
+++ 2.6 - Associação + Gerenciador + SRP/Ex 8/Album.java
@@ -1,1 +1,45 @@
-a
+package com.mycompany.exerciciooo;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Album {
+    private String nameOfTheAlbum;
+    private int yearOfLaunch;
+    private List<Musica> listOfMusics;
+    
+    public Album () {
+        nameOfTheAlbum = "";
+        yearOfLaunch = 0;
+        listOfMusics = new ArrayList <>();
+    }
+
+    public void addMusics (List listOfMusics) {
+        for (Musica music:listOfMusics) {
+            music.preencher();
+        }
+    }
+    
+    public String getNameOfTheAlbum() {
+        return nameOfTheAlbum;
+    }
+
+    public void setNameOfTheAlbum(String nameOfTheAlbum) {
+        this.nameOfTheAlbum = nameOfTheAlbum;
+    }
+
+    public int getYearOfLaunch() {
+        return yearOfLaunch;
+    }
+
+    public void setYearOfLaunch(int yearOfLaunch) {
+        this.yearOfLaunch = yearOfLaunch;
+    }
+
+    public List getListOfMusics() {
+        return listOfMusics;
+    }
+
+    public void setListOfMusics(List listOfMusics) {
+        this.listOfMusics = listOfMusics;
+    }
+}
