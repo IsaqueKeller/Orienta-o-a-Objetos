67749628ab269fcd55c8cff9e583c26d279240be
Isaque Keller <kellerisaque@gmail.com>
2026-04-24 16:06:23 -0300

    Create Transacao.java

---
+++ 2.6 - Associação + Gerenciador + SRP/Ex 11/Transacao.java
@@ -0,0 +1,14 @@
+package com.mycompany.associacaogerenciadorsrp;
+
+public class Transacao {
+    private String transactionIdentifier;
+    private String typeOfTransaction;
+    private double value;
+    private String date;
+    private Conta accountAssociated;
+    
+    
+    public Transacao () {
+        
+    }
+}
