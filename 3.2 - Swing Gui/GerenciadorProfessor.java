e86b209baf8958d987f15fcfd8108c0b9839db9a
Isaque Keller <kellerisaque@gmail.com>
2026-05-14 12:55:45 -0300

    Create GerenciadorProfessor.java

---
+++ 3.2 - Swing Gui/GerenciadorProfessor.java
@@ -0,0 +1,32 @@
+package com.mycompany.swinggui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GerenciadorProfessor {
+    private List<Professor> listOfTeachers;
+    
+    public GerenciadorProfessor () {
+        listOfTeachers = new ArrayList<>();
+    }
+    
+    public void addTeacher (Professor professor) {
+        listOfTeachers.add(professor);
+    }
+    
+    public void removeTeacher (String name) {
+        for (Professor professor : listOfTeachers) {
+            if (professor.getName().equals(name))   
+            listOfTeachers.remove(professor);
+        }
+    }
+    
+    public Professor searchTeacherByName (String name) {
+        for (Professor professor : listOfTeachers) {
+            if (professor.getName().equals(name)) {
+                return professor;
+        }
+    }
+        return null;
+}
+}
