250f921176c5660cd9574b097ea1ed472cb20ee9
Isaque Keller <kellerisaque@gmail.com>
2026-05-15 11:02:27 -0300

    Create SwingGui.java

---
+++ 3.2 - Swing Gui/Exercicio 2/SwingGui.java
@@ -0,0 +1,9 @@
+package com.mycompany.swinggui;
+
+public class SwingGui {
+
+    public static void main(String[] args) {
+        FrCadPessoa telaMenu = new FrCadPessoa();
+        telaMenu.setVisible(true);
+    }
+}
