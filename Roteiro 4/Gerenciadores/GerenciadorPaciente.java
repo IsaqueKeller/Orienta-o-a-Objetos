147b7a01fe72592f6070b7ad35a89cea9d4699cd
Isaque Keller <kellerisaque@gmail.com>
2026-05-15 16:53:51 -0300

    Create GerenciadorPaciente.java

---
+++ Roteiro 4/Gerenciadores/GerenciadorPaciente.java
@@ -0,0 +1,64 @@
+package com.mycompany.roteiro4;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class GerenciadorPaciente {
+    private Map <String , Paciente> pacientes;
+
+    public GerenciadorPaciente () {
+        pacientes = new HashMap <>();
+    }
+    
+    public void putPaciente (Paciente paciente) {
+        pacientes.put(paciente.getCpf(), paciente);
+    }
+    
+    public Paciente removePacienteByCpf (String cpf) {
+        return pacientes.remove(cpf);
+    }
+    
+    public Paciente searchPacienteByCpf (String cpf) {
+       return pacientes.get(cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.pacientes);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final GerenciadorPaciente other = (GerenciadorPaciente) obj;
+        return Objects.equals(this.pacientes, other.pacientes);
+    }
+
+    @Override
+    public String toString() {
+        return "GerenciadorPaciente{" + "pacientes=" + pacientes + '}';
+    }
+    
+    public int size () {
+        return this.pacientes.size();
+    }
+    
+    public boolean containsKey (String cpf) {
+        return this.pacientes.containsKey(cpf);
+    }
+    
+    public Map<String, Paciente> getPacientes() {
+        return pacientes;
+    }
+}
