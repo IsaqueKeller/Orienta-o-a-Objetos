946a4bee6bbf542d1b91a681ac18b1385f2a1a36
Isaque Keller <kellerisaque@gmail.com>
2026-05-15 16:54:09 -0300

    Create GerenciadorMedico.java

---
+++ Roteiro 4/Gerenciadores/GerenciadorMedico.java
@@ -0,0 +1,14 @@
+package com.mycompany.roteiro4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GerenciadorMedico {
+    private Map <String , Medico> medicos;
+    
+    public GerenciadorMedico () {
+        medicos = new HashMap <>();
+    }
+    
+    
+}
